56591991710a2ff6777bec9ec32e25a8a886d85f
Tfff1 <tfinlay2011@gmail.com>
2016-06-06 20:39:06 +1200

    Added basic catches.

--- js/tfff1s-multilingual.js
+++ js/tfff1s-multilingual.js
@@ -1,48 +1,65 @@
 //Global Variables:
 var attributeName;
-
-//$(window).ready(function () {
-//    changeLanguage($('html').attr('lang'));
-//});
+var debugMode = false;
 
 function matchesLangID(value) {
     return value === attributeName;
 }
 
-function changeLanguage(targetLang) {
-        
-    var targetLangFile = "language/" + targetLang + ".lang";
+function changeLanguage(targetLang, LocationOfLanguageFile, Debug) {
+    
+    debugMode = Debug;
+    
+    var targetLangFile = LocationOfLanguageFile + "/" +  targetLang + ".lang";
     console.log("Searching for lang file for " + targetLang + " at: " + targetLangFile);
     
     var xhr = new XMLHttpRequest();
 
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                // OK
+            } else {
+                // not OK
+                alert('Error: Failed to load. Request Returned: ' + xhr.responseText);
+            }
+        }
+    };
+    
     xhr.open('GET', targetLangFile, false);
     xhr.send(null);
     
     var string = xhr.responseText.split('\n');
     var langArray = string.concat();
 // `log` is the array of logs you want
-    console.log("Found Array: " + langArray);
+    log("Found Array: " + langArray);
     // Start changing Text:
     $("lang").each(function () {
-        console.log("gottem!");
+        log("Started work on new Lang Element");
         attributeName = $(this).attr('langID');
-        console.log("Attribute Name = " + attributeName);
+        log("Attribute Name = " + attributeName);
         
         var langLine = langArray.filter(function(str){
             return str.indexOf(attributeName) > -1
         });
                 
-        console.log("Found Lang Line for " + attributeName + " to be: " + langLine);
+        log("Found Lang Line for " + attributeName + " to be: " + langLine);
         
         var langLineString = langLine.toString();
         var langLineArray = langLineString.split('=');
         var langLineNoTitle = langLineArray.splice(1, 999999999);
         
-        console.log("Removed start of Line, now it looks like: " + langLineNoTitle);
+        log("Removed start of Line, now it looks like: " + langLineNoTitle);
         
         //Final Step - Display Text:
         $(this).html(langLineNoTitle);
     });
     console.log("Language Changes Complete!");
+}
+
+function log(message) { 
+    if (debugMode === true) {
+        console.log(message);
+    }
+    return null
 }
